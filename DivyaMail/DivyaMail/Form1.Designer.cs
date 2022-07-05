3906eed452fe952f401985fed247737d212e8ff2
DivyaAmmu22 <108730335+DivyaAmmu22@users.noreply.github.com>
2022-07-05 16:02:51 +0530

    Add files via upload

---
+++ DivyaMail/DivyaMail/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace DivyaMail
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnSndmail = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // BtnSndmail
+            // 
+            this.BtnSndmail.Location = new System.Drawing.Point(321, 215);
+            this.BtnSndmail.Name = "BtnSndmail";
+            this.BtnSndmail.Size = new System.Drawing.Size(75, 23);
+            this.BtnSndmail.TabIndex = 0;
+            this.BtnSndmail.Text = "Send Email";
+            this.BtnSndmail.UseVisualStyleBackColor = true;
+            this.BtnSndmail.Click += new System.EventHandler(this.BtnSndmail_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(367, 256);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 17);
+            this.label1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(715, 471);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.BtnSndmail);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnSndmail;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ DivyaMail/DivyaMail/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DivyaMail
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnSndmail_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (MailMessage mail = new MailMessage())
+                {
+                    mail.From = new MailAddress("grapigztics@gmail.com");
+                    mail.To.Add("stephenap@fourbends.com");
+                    mail.CC.Add("sdndinesh@fourbends.com");
+                    mail.Subject = "Divya P- Test mail using console application";
+                    mail.Body = "<h1>Hi,This mail is sent by using the console application</h1>";
+                    mail.IsBodyHtml = true;
+
+
+                    using (SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587))
+                    {
+                        smtp.Credentials = new System.Net.NetworkCredential("grapigztics@gmail.com", "Ktr1@hdsjc*^!");
+                        smtp.EnableSsl = true;
+                        smtp.Send(mail);
+                        label1.Text = "Mail Sent";
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                label1.Text = ex.Message;
+            }
+        }
+
+
+    }
+}
