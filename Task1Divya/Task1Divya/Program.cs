603d6dd29a40833643ddba7a95db2e027a6fa0cb
DivyaAmmu22 <108730335+DivyaAmmu22@users.noreply.github.com>
2022-07-05 21:35:34 +0530

    task 1

---
+++ Task1Divya/Task1Divya/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1Divya
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string source = @"D:\";
+            string Destin = @"E:\";
+
+            File.Copy(source + "testfile.txt", Destin + "testfile.txt");
+
+            Console.WriteLine("Done");
+            Console.ReadKey();
+        }
+    }
+}
